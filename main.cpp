7f3ee93b477aac189e3816836d5ebda75e1eaa96
reika727 <reikasuya727@gmail.com>
2020-08-13 08:33:56 +0900

    光源とドラッグ機能を追加

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "othello.hpp"
 #include "othello_algorithm.hpp"
 #include <algorithm>
+#include <cmath>
 #include <random>
 #include <thread>
 
@@ -11,9 +12,12 @@ enum class game_turn {
     player,
     enemy,
 };
-game_turn turn = game_turn::player;
-othello::board b;
-int mouse_x = 0, mouse_y = 0;
+static game_turn turn = game_turn::player;
+static othello::board b;
+static int mouse_x = 0, mouse_y = 0;
+static bool mouse_rb_on = false;
+static float theta = 45;
+static float phi = 60;
 void play_enemy_async()
 {
     std::thread p(
@@ -39,51 +43,94 @@ void resize(int w, int h)
 void display()
 {
     constexpr float box_size = 2;
-    auto draw_nth_boxs_outline = [](int n) {
-        const int x = (n - 1) % 8, y = (n - 1) / 8;
-        glPushMatrix();
-        glTranslated((x - 3.5) * box_size, (y - 3.5) * box_size, 0);
-        glBegin(GL_LINE_LOOP);
-        glVertex3f(-box_size / 2, -box_size / 2, -box_size / 2);
-        glVertex3f(-box_size / 2, +box_size / 2, -box_size / 2);
-        glVertex3f(+box_size / 2, +box_size / 2, -box_size / 2);
-        glVertex3f(+box_size / 2, -box_size / 2, -box_size / 2);
-        glEnd();
-        glPopMatrix();
-    };
+    constexpr GLfloat board_ambi[] = {0.0, 0.05, 0.0, 1.0};
+    constexpr GLfloat board_diff[] = {0.4, 0.5, 0.4, 1.0};
+    constexpr GLfloat board_spec[] = {0.04, 0.7, 0.04, 1.0};
+    constexpr GLfloat board_shine[] = {10.0};
+    constexpr GLfloat line_ambi[] = {0.0, 0.0, 0.0, 1.0};
+    constexpr GLfloat line_diff[] = {0.0, 0.0, 0.0, 1.0};
+    constexpr GLfloat line_spec[] = {0.0, 0.0, 0.0, 1.0};
+    constexpr GLfloat line_shine[] = {0.0};
+    constexpr GLfloat black_pot_ambi[] = {0.05375, 0.05, 0.06625, 1.0};
+    constexpr GLfloat black_pot_diff[] = {0.18275, 0.17, 0.22525, 1.0};
+    constexpr GLfloat black_pot_spec[] = {0.332741, 0.328634, 0.346435, 1.0};
+    constexpr GLfloat black_pot_shine[] = {38.4};
+    constexpr GLfloat white_pot_ambi[] = {0.25, 0.20725, 0.20725, 1.0};
+    constexpr GLfloat white_pot_diff[] = {1, 0.829, 0.829, 1.0};
+    constexpr GLfloat white_pot_spec[] = {0.296648, 0.296648, 0.296648, 1.0};
+    constexpr GLfloat white_pot_shine[] = {10.24};
+    constexpr double r = 50;
     glClearColor(1.0, 1.0, 1.0, 1.0);
     glClearStencil(0);
-    glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
     glLoadIdentity();
-    gluLookAt(30, 30, -30, 0, 0, 0, 0, -1, 0);
-    //gluLookAt(40, 0, -10, 0, 0, 0, 0, 0, -1);
-    //gluLookAt(0, 0, -30, 0, 0, 0, 0, -1, 0);
+    gluLookAt(
+        r * std::sin(phi / 180 * M_PI) * std::cos(theta / 180 * M_PI),
+        r * std::cos(phi / 180 * M_PI),
+        r * std::sin(phi / 180 * M_PI) * std::sin(theta / 180 * M_PI),
+        0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    glEnable(GL_DEPTH_TEST);
     glEnable(GL_STENCIL_TEST);
     glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+
+    glPushMatrix();
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, board_ambi);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, board_diff);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, board_spec);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, board_shine);
+    glTranslatef(0, -3.5 * box_size, 0);
     for (int y = 0; y < 8; ++y) {
+        glTranslatef(-3.5 * box_size, 0, 0);
         for (int x = 0; x < 8; ++x) {
             glStencilFunc(GL_ALWAYS, y * 8 + x + 1, 0xff);
-            glPushMatrix();
-            glTranslatef((x - 3.5) * box_size, (y - 3.5) * box_size, 0);
-            glColor3f(0.0, 1.0, 0.0);
             glutSolidCube(box_size);
-            glPopMatrix();
+            glTranslatef(box_size, 0, 0);
         }
+        glTranslatef(-4.5 * box_size, 0, 0);
+        glTranslatef(0, box_size, 0);
     }
-    for (int i = 1; i <= 64; ++i) {
-        glColor3d(0.0, 0.0, 0.0);
-        draw_nth_boxs_outline(i);
-    }
+    glPopMatrix();
+
+    glPushMatrix();
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, line_ambi);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, line_diff);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, line_spec);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, line_shine);
+    glTranslatef(0, -3.5 * box_size, 0);
     for (int y = 0; y < 8; ++y) {
+        glTranslatef(-3.5 * box_size, 0, 0);
+        for (int x = 0; x < 8; ++x) {
+            glBegin(GL_LINE_LOOP);
+            glVertex3f(-box_size / 2, -box_size / 2, -box_size / 2);
+            glVertex3f(-box_size / 2, +box_size / 2, -box_size / 2);
+            glVertex3f(+box_size / 2, +box_size / 2, -box_size / 2);
+            glVertex3f(+box_size / 2, -box_size / 2, -box_size / 2);
+            glEnd();
+            glTranslatef(box_size, 0, 0);
+        }
+        glTranslatef(-4.5 * box_size, 0, 0);
+        glTranslatef(0, box_size, 0);
+    }
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0, -3.5 * box_size, -box_size);
+    for (int y = 0; y < 8; ++y) {
+        glTranslatef(-3.5 * box_size, 0, 0);
         for (int x = 0; x < 8; ++x) {
             if (auto s = b(x, y)) {
-                glPushMatrix();
                 if (s == othello::stone::black()) {
-                    glColor3f(0, 0, 0);
-                } else if (s == othello::stone::white()) {
-                    glColor3f(1, 1, 1);
+                    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, black_pot_ambi);
+                    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, black_pot_diff);
+                    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, black_pot_spec);
+                    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, black_pot_shine);
+                } else {
+                    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, white_pot_ambi);
+                    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, white_pot_diff);
+                    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, white_pot_spec);
+                    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, white_pot_shine);
                 }
-                glTranslatef((x - 3.5) * box_size, (y - 3.5) * box_size, -box_size / 2);
+                glPushMatrix();
                 glRotatef(-90, 1, 0, 0);
                 std::random_device seed_gen;
                 std::default_random_engine engine(seed_gen());
@@ -91,15 +138,14 @@ void display()
                 glutSolidTeapot(0.5);
                 glPopMatrix();
             }
+            glTranslatef(box_size, 0, 0);
         }
+        glTranslatef(-4.5 * box_size, 0, 0);
+        glTranslatef(0, box_size, 0);
     }
-    unsigned int index;
-    glReadPixels(mouse_x, glutGet(GLUT_WINDOW_HEIGHT) - mouse_y, 1, 1, GL_STENCIL_INDEX, GL_UNSIGNED_INT, &index);
-    if (index != 0) {
-        glColor3d(1.0, 0.0, 0.0);
-        draw_nth_boxs_outline(index);
-    }
-    glFlush();
+    glPopMatrix();
+
+    glutSwapBuffers();
 }
 void mouse_moves(int x, int y)
 {
@@ -109,6 +155,9 @@ void mouse_moves(int x, int y)
 }
 void mouse_click(int button, int state, int x, int y)
 {
+    if (button == GLUT_RIGHT_BUTTON) {
+        mouse_rb_on = state == GLUT_DOWN;
+    }
     if (turn == game_turn::enemy) return;
     int index;
     glReadPixels(mouse_x, glutGet(GLUT_WINDOW_HEIGHT) - mouse_y, 1, 1, GL_STENCIL_INDEX, GL_INT, &index);
@@ -126,16 +175,44 @@ void mouse_click(int button, int state, int x, int y)
         play_enemy_async();
     }
 }
+void drag_motion(int x, int y)
+{
+    if (mouse_rb_on) {
+        theta += x - mouse_x;
+        phi = std::clamp(phi + mouse_y - y, 0.1f, 179.9f);
+        mouse_x = x;
+        mouse_y = y;
+    }
+    glutPostRedisplay();
+}
+void lightInit(void)
+{
+    constexpr GLfloat light0pos[] = {0.0, 0.0, -10.0, 1.0};
+    constexpr GLfloat light0ambi[] = {0.2, 0.2, 0.2, 1.0};
+    constexpr GLfloat light0diff[] = {0.8, 0.8, 0.8, 1.0};
+    constexpr GLfloat light0spec[] = {0.5, 0.5, 0.5, 1.0};
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_NORMALIZE);
+    glLightfv(GL_LIGHT0, GL_POSITION, light0pos);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light0ambi);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light0diff);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light0spec);
+    //glLightf(GL_LIGHT0, GL_SPOT_CUTOFF,  10.0 );
+    glShadeModel(GL_SMOOTH);
+}
 int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGBA | GLUT_STENCIL);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH | GLUT_STENCIL);
     glutInitWindowSize(640, 480);
     glutCreateWindow("window");
     glutDisplayFunc(display);
     glutReshapeFunc(resize);
     glutPassiveMotionFunc(mouse_moves);
+    glutMotionFunc(drag_motion);
     glutMouseFunc(mouse_click);
+    lightInit();
     glutMainLoop();
     return 0;
 }
